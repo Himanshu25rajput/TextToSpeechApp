b004836ca5c07491c26fcd16998beb633c8a9cad
Himanshu25rajput <83720714+Himanshu25rajput@users.noreply.github.com>
2022-03-31 22:21:36 +0530

    TextToSpeech

---
+++ app/src/main/java/com/example/texttospeech/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.texttospeech
+
+import android.app.ActivityManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Toast
+import com.example.texttospeech.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    private var tts: TextToSpeech?=null
+    private var binding:ActivityMainBinding?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        tts=TextToSpeech(this,this)
+        binding?.btStart?.setOnClickListener{view ->
+            if (binding?.edittext?.text!!.isEmpty()){
+                Toast.makeText(this, "Enter a text to Speech", Toast.LENGTH_SHORT).show()
+            }else{
+                speakOut(binding?.edittext?.text.toString())
+            }
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status==TextToSpeech.SUCCESS){
+            val result=tts?.setLanguage(Locale.US)
+
+            if(result==TextToSpeech.LANG_MISSING_DATA ||
+                result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("TTS","The language specified is not supported")
+            }else{
+                Log.e("TTS","Initialization failed")
+            }
+        }
+    }
+    private fun speakOut(text:String){
+        tts?.speak(text,TextToSpeech.QUEUE_FLUSH,null,"")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (tts!=null){
+            tts?.stop()
+            tts?.shutdown()
+        }
+        binding =null
+    }
+}
